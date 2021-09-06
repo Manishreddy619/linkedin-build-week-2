8353b7e30dfc4270bfa244b4cc304900c17cc1f2
HajnalAron <79903244+HajnalAron@users.noreply.github.com>
2021-09-06 14:36:30 +0200

    Added profile model

--- src/Utilities/fetches.js
+++ src/Utilities/fetches.js
@@ -105,6 +105,21 @@ export const getFilteredProfiles = async (filterString) => {
 };
 
 export const updateMyProfile = async (profileData) => {
+  // PROFILE Model:
+  // {
+  //     "_id": "5d84937322b7b54d848eb41b", //server generated
+  //     "name": "Diego",
+  //     "surname": "Banovaz",
+  //     "email": "diego@strive.school",
+  //     "bio": "SW ENG",
+  //     "title": "COO @ Strive School",
+  //     "area": "Berlin",
+  //     "image": ..., //server generated on upload
+  //     "username": "admin", //server generated from Auth
+  //     "createdAt": "2019-09-20T08:53:07.094Z", //server generated
+  //     "updatedAt": "2019-09-20T09:00:46.977Z", //server generated
+  //     "__v": 0 //server generated
+  // }
   try {
     const apiResp = await fetch(apiUrl, {
       method: "PUT",
