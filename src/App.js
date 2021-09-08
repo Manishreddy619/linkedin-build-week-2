cb80f27d2761d75a01885666e4bf4ef0d4448cb0
Manish Elaganti <65448066+Manishreddy619@users.noreply.github.com>
2021-09-08 20:20:01 +0200

    Revert "Merge branch 'Devlop' into Yan"
    This reverts commit efff1de32511e62e8f3b4384af3e8d7a4bdfdd74, reversing
    changes made to 6b1a7beb0e47794aa4941929149cad8970fb326b.

--- src/App.js
+++ src/App.js
@@ -4,21 +4,14 @@ import RightBar from './components/RightBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { UserDetails } from './components/OtherUserDetails';
-import AppNavigation from './components/AppNavigation.jsx';
-
-import Feed from './components/Feed';
-import Footer from './components/Footer';
+import LeftFeedProfile from './components/LeftFeedProfile';
 function App() {
 	return (
 		<Router>
 			<div className='app'>
-				<AppNavigation />
-				<Footer />
+				{/* header component */}
 				<div className='profileBody'>
 					<Route path='/' exact>
-						<Feed />
-					</Route>
-					<Route path='/me' exact>
 						<ProfileDetails />
 						<RightBar />
 					</Route>
