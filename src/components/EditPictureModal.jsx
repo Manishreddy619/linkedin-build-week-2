2f138a91407e7d22ce0180a1f45d0146fc0a6bfe
HajnalAron <79903244+HajnalAron@users.noreply.github.com>
2021-09-10 15:27:09 +0200

    Edit post state management plus create post fix

--- src/components/EditPictureModal.jsx
+++ src/components/EditPictureModal.jsx
@@ -1,113 +1,117 @@
-import React from 'react';
-import { Button, Modal, Form } from 'react-bootstrap';
-import { useState } from 'react';
+import React from "react";
+import { Button, Modal, Form } from "react-bootstrap";
+import { useState } from "react";
 import {
-	updatePost,
-	uploadPostPicture,
-	deletePost,
-} from '../Utilities/fetches';
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
-import DeleteIcon from '@material-ui/icons/Delete';
-import SendIcon from '@material-ui/icons/Send';
-const EditPictureModal = ({ id, message }) => {
-	const [show, setShow] = useState(false);
-	const handleClose = () => setShow(false);
-	const handleShow = () => setShow(true);
-	const [post, setPost] = useState({
-		text: message,
-	});
-	const [file, setFile] = useState(null);
-	const handleInput = (e, propertyName) => {
-		setPost({
-			...post,
+  updatePost,
+  uploadPostPicture,
+  deletePost
+} from "../Utilities/fetches";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import DeleteIcon from "@material-ui/icons/Delete";
+import SendIcon from "@material-ui/icons/Send";
+const EditPictureModal = ({ id, message, loadingState }) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const [post, setPost] = useState({
+    text: message
+  });
+  const [file, setFile] = useState(null);
+  const handleInput = (e, propertyName) => {
+    setPost({
+      ...post,
 
-			[propertyName]: e.target.value,
-		});
-	};
-	const deletePostItem = async (e) => {
-		e.preventDefault();
-		await deletePost(id);
-		handleClose();
-	};
-	const Sendpost = async (e) => {
-		await updatePost(id, post);
+      [propertyName]: e.target.value
+    });
+  };
+  const deletePostItem = async (e) => {
+    e.preventDefault();
+    await deletePost(id);
+    handleClose();
+    loadingState(true);
+  };
+  const Sendpost = async (e) => {
+    await updatePost(id, post);
 
-		setPost({
-			text: '',
-		});
-	};
-	const fileUpLoadHandler = async (e) => {
-		Sendpost();
-		e.preventDefault();
-		if (!file) {
-			handleClose();
-		}
-		await uploadPostPicture(id, file);
-		handleClose();
-	};
-	console.log(id);
+    setPost({
+      text: ""
+    });
+    loadingState(true);
+  };
+  const fileUpLoadHandler = async (e) => {
+    Sendpost();
+    e.preventDefault();
+    if (!file) {
+      handleClose();
+    }
+    await uploadPostPicture(id, file);
+    handleClose();
+    loadingState(true);
+  };
+  console.log(id);
 
-	return (
-		<div>
-			<p variant='primary' onClick={handleShow}>
-				<MoreHorizIcon className='horizontalDots' />
-			</p>
+  return (
+    <div>
+      <p variant="primary" onClick={handleShow}>
+        <MoreHorizIcon className="horizontalDots" />
+      </p>
 
-			<Modal show={show} onHide={handleClose}>
-				<Modal.Header closeButton>
-					<Modal.Title>edit post</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>
-					<Form>
-						<Form.Group className='mb-3'>
-							<Form.Label>write something</Form.Label>
-							<Form.Control
-								className='border-0'
-								as='textarea'
-								placeholder='What do you want to talk about ?'
-								value={post.text}
-								onChange={(e) => handleInput(e, 'text')}
-							/>
-						</Form.Group>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>edit post</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>write something</Form.Label>
+              <Form.Control
+                className="border-0"
+                as="textarea"
+                placeholder="What do you want to talk about ?"
+                value={post.text}
+                onChange={(e) => handleInput(e, "text")}
+              />
+            </Form.Group>
 
-						{/* <Button variant='success'>
+            {/* <Button variant='success'>
 							Send post
 						</Button> */}
-					</Form>
-					<Form>
-						<Form.Group className='mb-3'>
-							{/* <Form.Label>Choose image </Form.Label> */}
-							<Form.Control
-								className='border-0'
-								type='file'
-								placeholder='Upload a image '
-								onChange={(e) => {
-									setFile(e.target.files[0]);
-									console.log(e.target.files);
-									console.log(file);
-								}}
-							/>
-						</Form.Group>
-						<Button
-							variant='danger'
-							type='submit'
-							className='mx-3'
-							onClick={deletePostItem}>
-							<DeleteIcon /> delete post
-						</Button>
-						<Button variant='success' type='submit' onClick={fileUpLoadHandler}>
-							<SendIcon /> send post
-						</Button>
-					</Form>
-				</Modal.Body>
-				<Modal.Footer>
-					<Button variant='secondary' onClick={handleClose}>
-						Close
-					</Button>
-				</Modal.Footer>
-			</Modal>
-		</div>
-	);
+          </Form>
+          <Form>
+            <Form.Group className="mb-3">
+              {/* <Form.Label>Choose image </Form.Label> */}
+              <Form.Control
+                className="border-0"
+                type="file"
+                placeholder="Upload a image "
+                onChange={(e) => {
+                  setFile(e.target.files[0]);
+                  console.log(e.target.files);
+                  console.log(file);
+                }}
+              />
+            </Form.Group>
+            <Button
+              variant="danger"
+              type="submit"
+              className="mx-3"
+              onClick={deletePostItem}
+            >
+              <DeleteIcon /> delete post
+            </Button>
+            <Button variant="success" type="submit" onClick={fileUpLoadHandler}>
+              <SendIcon /> send post
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 };
 
 export default EditPictureModal;
--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,128 +1,136 @@
-import React from 'react';
-import './PostCard.css';
-import { getPosts } from '../Utilities/fetches';
+import React from "react";
+import "./PostCard.css";
+import { getPosts } from "../Utilities/fetches";
 
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
-import ThumbUpIcon from '@material-ui/icons/ThumbUp';
-import ShareIcon from '@material-ui/icons/Share';
-import CommentIcon from '@material-ui/icons/Comment';
-import SendIcon from '@material-ui/icons/Send';
-import { useEffect, useState } from 'react';
-import { Spinner, Button, Modal } from 'react-bootstrap';
-import EditPictureModal from './EditPictureModal';
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import ShareIcon from "@material-ui/icons/Share";
+import CommentIcon from "@material-ui/icons/Comment";
+import SendIcon from "@material-ui/icons/Send";
+import { useEffect, useState } from "react";
+import { Spinner, Button, Modal } from "react-bootstrap";
+import EditPictureModal from "./EditPictureModal";
 
 export default function PostCard() {
-	const [posts, setPosts] = useState([]);
-	const [isLoading, setLoading] = useState(true);
-	const [isTextExpanded, setTextExpanded] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [isTextExpanded, setTextExpanded] = useState(false);
 
-	const [myPost, setMypost] = useState(null);
+  const [myPost, setMypost] = useState(null);
 
-	let myId = '6135d7437be6c10015f9db99';
-	const fetchPosts = async () => {
-		const fetchedPosts = await getPosts();
-		setPosts(fetchedPosts?.reverse());
-		setLoading(false);
-	};
-	const updateMYpicture = async (e, singlePost) => {
-		// console.log(e);
-		// console.log(singlePost.user?._id);
-		// posts
-		// .filter((post) => post.user?._id === '6135d7437be6c10015f9db99')
-		// .map((b) => {
-		// 	// return b && <div>{console.log(b)}</div>;
-		// 	return '';
-		// });
+  let myId = "6135d7437be6c10015f9db99";
+  const fetchPosts = async () => {
+    const fetchedPosts = await getPosts();
+    setPosts(fetchedPosts?.reverse());
+    setLoading(false);
+  };
+  const updateMYpicture = async (e, singlePost) => {
+    // console.log(e);
+    // console.log(singlePost.user?._id);
+    // posts
+    // .filter((post) => post.user?._id === '6135d7437be6c10015f9db99')
+    // .map((b) => {
+    // 	// return b && <div>{console.log(b)}</div>;
+    // 	return '';
+    // });
 
-		if (singlePost.user?._id === myId) {
-			console.log(singlePost);
-			setMypost(singlePost);
-		}
-	};
-	console.log(posts);
-	useEffect(() => {
-		fetchPosts();
-	}, []);
+    if (singlePost.user?._id === myId) {
+      console.log(singlePost);
+      setMypost(singlePost);
+    }
+  };
+  console.log(posts);
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
-	return (
-		<div>
-			{isLoading && <Spinner animation='grow' />}
-			{posts &&
-				posts.slice(0, 70).map((post) => (
-					<div key={post._id} className='postCard d-flex flex-column'>
-						<div className='postCardTop d-flex justify-content-between'>
-							<div className='d-flex justify-content-center align-items-center'>
-								<img
-									src={post.user.image}
-									alt=''
-									srcset=''
-									height='48px'
-									width='48px'
-								/>
-								<div className='ml-1'>
-									<div className='profileName'>
-										{post.user.name} {post.user.surname}
-									</div>
-									<div className='profileMuted text-muted'>
-										Created at: {new Date(post.createdAt).toLocaleString()}
-									</div>
-									<div className='profileMuted text-muted'>
-										Updated at: {new Date(post.updatedAt).toLocaleString()}
-									</div>
-								</div>
-							</div>
-							<div onClick={(e) => updateMYpicture(e, post)}>
-								{post.user._id !== myId && (
-									<MoreHorizIcon className='horizontalDots' />
-								)}
-							</div>
-							{post.user._id === myId && (
-								<EditPictureModal id={post._id} message={post.text} />
-							)}
-						</div>
-						<div className='postCardMiddle d-flex flex-column'>
-							<div className='postCardMiddle'>{post.text}</div>
-							<a className='align-self-end postCardMiddle'>see more...</a>
-							{post.image && (
-								<img
-									src={post.image}
-									alt=''
-									srcset=''
-									width='540px'
-									height='285px'
-								/>
-							)}
-							{post.image === undefined && (
-								<img
-									src='https://picsum.photos/540/285'
-									alt=''
-									srcset=''
-									width='540px'
-									height='285px'
-								/>
-							)}
-						</div>
-						<div className='postCardBottom d-flex flex-wrap justify-content-between w-100'>
-							<hr className='postCardLine' />
-							<div className='d-flex align-items-center justify-content-center bottomIcons'>
-								<ThumbUpIcon className='postCardIcons' />
-								<div className='postCardIcon'>Like</div>
-							</div>
-							<div className='d-flex align-items-center justify-content-center bottomIcons "'>
-								<CommentIcon className='postCardIcons' />
-								<div className='postCardIcon'>Comment</div>
-							</div>
-							<div className='d-flex align-items-center justify-content-center bottomIcons "'>
-								<ShareIcon className='postCardIcons' />
-								<div className='postCardIcon'>Share</div>
-							</div>
-							<div className='d-flex align-items-center justify-content-center bottomIcons "'>
-								<SendIcon className='postCardIcons' />
-								<div className='postCardIcon'>Send</div>
-							</div>
-						</div>
-					</div>
-				))}
-		</div>
-	);
+  useEffect(() => {
+    if (isLoading === true) fetchPosts();
+  }, [isLoading]);
+
+  return (
+    <div>
+      {isLoading && <Spinner className="mx-auto" animation="grow" />}
+      {posts &&
+        posts.slice(0, 70).map((post) => (
+          <div key={post._id} className="postCard d-flex flex-column">
+            <div className="postCardTop d-flex justify-content-between">
+              <div className="d-flex justify-content-center align-items-center">
+                <img
+                  src={post.user.image}
+                  alt=""
+                  srcset=""
+                  height="48px"
+                  width="48px"
+                />
+                <div className="ml-1">
+                  <div className="profileName">
+                    {post.user.name} {post.user.surname}
+                  </div>
+                  <div className="profileMuted text-muted">
+                    Created at: {new Date(post.createdAt).toLocaleString()}
+                  </div>
+                  <div className="profileMuted text-muted">
+                    Updated at: {new Date(post.updatedAt).toLocaleString()}
+                  </div>
+                </div>
+              </div>
+              <div onClick={(e) => updateMYpicture(e, post)}>
+                {post.user._id !== myId && (
+                  <MoreHorizIcon className="horizontalDots" />
+                )}
+              </div>
+              {post.user._id === myId && (
+                <EditPictureModal
+                  loadingState={(state) => setLoading(state)}
+                  id={post._id}
+                  message={post.text}
+                />
+              )}
+            </div>
+            <div className="postCardMiddle d-flex flex-column">
+              <div className="postCardMiddle">{post.text}</div>
+              <a className="align-self-end postCardMiddle">...see more</a>
+              {post.image && (
+                <img
+                  src={post.image}
+                  alt=""
+                  srcset=""
+                  width="540px"
+                  height="285px"
+                />
+              )}
+              {post.image === undefined && (
+                <img
+                  src="https://picsum.photos/540/285"
+                  alt=""
+                  srcset=""
+                  width="540px"
+                  height="285px"
+                />
+              )}
+            </div>
+            <div className="postCardBottom d-flex flex-wrap justify-content-between w-100">
+              <hr className="postCardLine" />
+              <div className="d-flex align-items-center justify-content-center bottomIcons">
+                <ThumbUpIcon className="postCardIcons" />
+                <div className="postCardIcon">Like</div>
+              </div>
+              <div className='d-flex align-items-center justify-content-center bottomIcons "'>
+                <CommentIcon className="postCardIcons" />
+                <div className="postCardIcon">Comment</div>
+              </div>
+              <div className='d-flex align-items-center justify-content-center bottomIcons "'>
+                <ShareIcon className="postCardIcons" />
+                <div className="postCardIcon">Share</div>
+              </div>
+              <div className='d-flex align-items-center justify-content-center bottomIcons "'>
+                <SendIcon className="postCardIcons" />
+                <div className="postCardIcon">Send</div>
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 }
